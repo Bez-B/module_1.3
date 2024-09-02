0eac124eaff2ba6e0b791a6458d4ec3505d23ec3
Bez-B <79119302066@yandex.ru>
2024-09-02 23:28:34 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Alexandr"
+print('My name is ' + name)
+age = 39
+print("I'm", age)
+age = age + 1
+print('I will be', age, 'years old soon')
+is_student = True
+print("I'm a student:", is_student)
